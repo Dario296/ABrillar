c479cf860fc52c4e243f2689f82ea0a30bb1720c
Dario296 <dariorodriguez296@yahoo.com.ar>
2024-09-06 14:27:34 -0300

    agrego buscadores a la partes de ventas

--- src/component/admin/salesOnCredit/SalesList.js
+++ src/component/admin/salesOnCredit/SalesList.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import OnCreditProductSale from './ProductSale';
 import useProductsList from '../../hooks/ProductsList';
-import { CircularProgress} from '@mui/material';
+import { CircularProgress, Fab, TextField, Box, Modal } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 import Grid from '@mui/material/Grid2';
 
 const SalesList = () => {
-	const { productsList, loading} = useProductsList();
+	const { productsList, loading } = useProductsList();
+	const [searchTerm, setSearchTerm] = useState('');
+	const [open, setOpen] = useState(false);
+
+	const handleOpen = () => {setSearchTerm('');setOpen(true);}
+	const handleClose = () => setOpen(false);
+
+	const handleSearch = (e) => {
+		setSearchTerm(e.target.value.toLowerCase());
+	};
+
+	const filteredProducts = productsList.filter((product) =>
+		product.nombre.toLowerCase().includes(searchTerm)
+	);
+
 	productsList.sort((a, b) => a.nombre.localeCompare(b.nombre)); // Ordena productos por nombre
 
 	if (loading) {
@@ -31,11 +46,30 @@ const SalesList = () => {
 					</tr>
 				</thead>
 				<tbody>
-					{productsList.map((product) => (
-						<OnCreditProductSale key={product.ID} product={product}/>
+					{filteredProducts.map((product) => (
+						<OnCreditProductSale key={product.ID} product={product} />
 					))}
 				</tbody>
 			</Table>
+
+			{/* Botón flotante */}
+			<Fab color="primary" aria-label="search" onClick={handleOpen} style={{ position: 'fixed', bottom: 16, right: 16 }}>
+				<SearchIcon />
+			</Fab>
+
+			{/* Modal de búsqueda */}
+			<Modal open={open} onClose={handleClose}>
+				<Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 300, bgcolor: 'background.paper', p: 4, borderRadius: 2, boxShadow: 24 }}>
+					<TextField
+						autoFocus
+						fullWidth
+						label="Buscar Producto"
+						variant="outlined"
+						onChange={handleSearch}
+						placeholder="Nombre del producto"
+					/>
+				</Box>
+			</Modal>
 		</>
 	);
 };
